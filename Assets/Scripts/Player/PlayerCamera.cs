5dfadaaf5d32737bf687c6538ae17b804b26f92b
Lalalaa236 <nhankiet236@gmail.com>
2025-09-02 20:13:19 +0700

    feat: add rotation to player

--- Assets/Scripts/Player/PlayerCamera.cs
+++ Assets/Scripts/Player/PlayerCamera.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class PlayerCamera : MonoBehaviour
 {
     public static PlayerCamera instance;
+    public Camera playerCamera;
 
     private void Awake()
     {
--- Assets/Scripts/Player/PlayerMovementManager.cs
+++ Assets/Scripts/Player/PlayerMovementManager.cs
@@ -12,39 +12,16 @@ public class PlayerMovementManager : MonoBehaviour
     public float horizontalMovement;
     public float moveAmount;
 
-    private Vector3 moveDirection;
+    private Vector3 moveDir;
+    private Vector3 targetDir;
 
     [SerializeField] private float walkingSpeed = 2f;
     [SerializeField] private float sprintingSpeed = 5f;
+    [SerializeField] private float rotationSpeed = 10f;
 
     private void Awake()
     {
         playerManager = GetComponent<PlayerManager>();
-    }
-
-    public void HandleMovement()
-    {
-        // Handle player movement input and apply it to the player character
-        HandleGroundedMovement();
-    }
-
-    private void HandleGroundedMovement()
-    {
-        GetVerticalAndHorizontalInput();
-        
-        moveDirection = PlayerCamera.instance.transform.forward * verticalMovement;
-        moveDirection += PlayerCamera.instance.transform.right * horizontalMovement;
-        moveDirection.Normalize();
-        moveDirection.y = 0;
-
-        if (InputManager.instance.moveAmount > 0.5)
-        {
-            playerManager.characterController.Move(moveDirection * sprintingSpeed * Time.deltaTime);
-        }
-        else if (InputManager.instance.moveAmount <= 0.5f)
-        {
-            playerManager.characterController.Move(moveDirection * walkingSpeed * Time.deltaTime);
-        }
     }
 
     private void GetVerticalAndHorizontalInput()
@@ -52,5 +29,49 @@ public class PlayerMovementManager : MonoBehaviour
         verticalMovement = InputManager.instance.verticalMovement;
         horizontalMovement = InputManager.instance.horizontalMovement;
         moveAmount = InputManager.instance.moveAmount;
+    }
+
+    public void HandleMovement()
+    {
+        // Handle player movement input and apply it to the player character
+        HandleGroundedMovement();
+        HandleRotation();
+    }
+
+    private void HandleGroundedMovement()
+    {
+        GetVerticalAndHorizontalInput();
+
+        moveDir = PlayerCamera.instance.transform.forward * verticalMovement;
+        moveDir += PlayerCamera.instance.transform.right * horizontalMovement;
+        moveDir.Normalize();
+        moveDir.y = 0;
+
+        if (InputManager.instance.moveAmount > 0.5)
+        {
+            playerManager.characterController.Move(moveDir * sprintingSpeed * Time.deltaTime);
+        }
+        else if (InputManager.instance.moveAmount <= 0.5f)
+        {
+            playerManager.characterController.Move(moveDir * walkingSpeed * Time.deltaTime);
+        }
+    }
+
+    private void HandleRotation()
+    {
+        targetDir = Vector3.zero;
+        targetDir = PlayerCamera.instance.playerCamera.transform.forward * verticalMovement;
+        targetDir += PlayerCamera.instance.playerCamera.transform.right * horizontalMovement;
+        targetDir.Normalize();
+        targetDir.y = 0;
+
+        if (targetDir == Vector3.zero)
+        {
+            targetDir = transform.forward;
+        }
+        Quaternion newRotation = Quaternion.LookRotation(targetDir);
+        Quaternion targetRotation = Quaternion.Slerp(transform.rotation, newRotation, rotationSpeed * Time.deltaTime);
+
+        transform.rotation = targetRotation;
     }
 }
