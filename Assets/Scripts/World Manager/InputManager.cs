483d6a5f16fe7c4eba511ce6c423ded89e80a679
Lalalaa 236 <nhankiet236@gmail.com>
2025-09-02 15:25:20 +0700

    feat: add input manager

---
+++ Assets/Scripts/World Manager/InputManager.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class InputManager : MonoBehaviour
+{
+    public static InputManager instance;
+    GameInput inputActions;
+
+    [SerializeField] Vector2 movementVector;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+            return;
+        }
+        SceneManager.activeSceneChanged += OnSceneChanged;
+    }
+
+    private void Start()
+    {
+        instance.enabled = false;
+    }
+
+    private void OnSceneChanged(Scene current, Scene next)
+    {
+        if (next.buildIndex == WorldSaveGameManager.instance.GetWorldSceneIndex())
+        {
+            instance.enabled = true;
+        }
+        else
+        {
+            instance.enabled = false;
+        }
+    }
+
+    private void OnEnable()
+    {
+        if (inputActions == null)
+        {
+            inputActions = new GameInput();
+            inputActions.Movement.Move.performed += i => movementVector = i.ReadValue<Vector2>();
+        }
+        inputActions.Enable();
+    }
+
+    private void OnDestroy()
+    {
+        SceneManager.activeSceneChanged -= OnSceneChanged;        
+    }
+}
